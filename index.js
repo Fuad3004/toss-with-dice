1e093d1b96ec89e20dda025672b8937a7eb99ffc
fuad3004 <fuadbinahmed21@gmail.com>
2023-05-08 19:57:24 +0600

    Learning Website

---
+++ index.js
@@ -0,0 +1,39 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage= "dice"+ randomNumber1+ ".png";
+
+var randomImageSource= "Images/" + randomDiceImage //Images/dice1.png
+
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2= "dice"+ randomNumber2+ ".png";
+
+var randomImageSource2= "Images/" + randomDiceImage2 //Images/dice1.png
+
+
+// var image2=document.querySelectorAll("img")[1]; 
+
+// image2.setAttribute("src", randomImageSource2);
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2); 
+
+
+
+if (randomNumber1> randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
+} 
+
+else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 wins🚩"
+}
+
+else {
+    document.querySelector("h1").innerHTML="Draw!"
+}
